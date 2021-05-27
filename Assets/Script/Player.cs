1ade0bbe8982e964b6ba929fcc7ea74aa6800cac
연제무 <brian2189@hanmail.net>
2021-05-27 11:47:11 +0900

    첫번째 수업 커밋

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 vector3 = new Vector3();
+
+        float playerSpeed = h * moveSpeed * Time.deltaTime;
+
+        vector3.x = playerSpeed;
+
+        transform.Translate(vector3);
+
+        if (h < 0)
+        {
+            this.GetComponent<Animator>().SetBool("Walk", true);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (h == 0)
+        {
+            this.GetComponent<Animator>().SetBool("Wallk", false);
+        }
+        else 
+        {
+            this.GetComponent<Animator>().SetBool("Wallk", true);
+
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+
+    }
+}
